2f7de3e346e4d0af0d81cc93d3cec376dae16fcf
Roland Krüger <roland.krueger@oio.de>
2016-01-25 13:34:33 +0100

    Add second string parameter to tests

--- src/test/java/org/roklib/webapps/uridispatching/parameter/value/ConsumedParameterValuesTest.java
+++ src/test/java/org/roklib/webapps/uridispatching/parameter/value/ConsumedParameterValuesTest.java
@@ -16,39 +16,43 @@ import static org.junit.Assert.assertThat;
 public class ConsumedParameterValuesTest {
 
     private ConsumedParameterValues values;
-    private SingleStringURIParameter stringParameter;
+    private SingleStringURIParameter stringTextParameter;
+    private SingleStringURIParameter stringNameParameter;
     private SingleIntegerURIParameter integerParameter;
 
     @Before
     public void setUp() {
         values = new ConsumedParameterValues();
-        stringParameter = new SingleStringURIParameter("text");
+        stringTextParameter = new SingleStringURIParameter("text");
+        stringNameParameter = new SingleStringURIParameter("name");
         integerParameter = new SingleIntegerURIParameter("number");
     }
 
     @Test
     public void returns_empty_optional_for_unknown_mapper_name() {
-        assertThat(values.getValueFor("unknown", stringParameter), equalTo(Optional.empty()));
+        assertThat(values.getValueFor("unknown", stringTextParameter), equalTo(Optional.empty()));
     }
 
     @Test
     public void returns_empty_optional_for_unavailable_parameter() {
-        values.setValueFor("first", stringParameter, "test");
+        values.setValueFor("first", stringTextParameter, "test");
         assertThat(values.getValueFor("first", integerParameter), equalTo(Optional.empty()));
     }
 
     @Test
     public void getValueFor_returns_correct_value() {
-        values.setValueFor("first", stringParameter, "test");
+        values.setValueFor("first", stringTextParameter, "textValue");
+        values.setValueFor("first", stringNameParameter, "nameValue");
         values.setValueFor("second", integerParameter, 17);
 
-        assertThat(values.getValueFor("first", stringParameter).get().getValue(), equalTo("test"));
+        assertThat(values.getValueFor("first", stringTextParameter).get().getValue(), equalTo("textValue"));
+        assertThat(values.getValueFor("first", stringNameParameter).get().getValue(), equalTo("nameValue"));
         assertThat(values.getValueFor("second", integerParameter).get().getValue(), equalTo(17));
     }
 
     @Test(expected = NullPointerException.class)
     public void getValueFor_mapper_name_null_not_allowed() {
-        values.getValueFor(null, stringParameter);
+        values.getValueFor(null, stringTextParameter);
     }
 
     @Test(expected = NullPointerException.class)
@@ -58,7 +62,7 @@ public class ConsumedParameterValuesTest {
 
     @Test(expected = NullPointerException.class)
     public void setValueFor_mapper_name_null_not_allowed() {
-        values.setValueFor(null, stringParameter, "");
+        values.setValueFor(null, stringTextParameter, "");
     }
 
     @Test(expected = NullPointerException.class)
