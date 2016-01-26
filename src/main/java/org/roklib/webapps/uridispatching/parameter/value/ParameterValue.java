2519abe3ae55a86b32b467ae6f7f7534b62eaf4a
Roland Krüger <info@rolandkrueger.info>
2016-01-26 14:45:26 +0100

    Disallow setting null values

--- src/main/java/org/roklib/webapps/uridispatching/parameter/value/ParameterValue.java
+++ src/main/java/org/roklib/webapps/uridispatching/parameter/value/ParameterValue.java
@@ -1,5 +1,7 @@
 package org.roklib.webapps.uridispatching.parameter.value;
 
+import org.roklib.webapps.uridispatching.helper.Preconditions;
+
 /**
  * Contains a single parameter value which has been collected during the interpretation process for a concrete URI
  * fragment.
@@ -10,6 +12,7 @@ public class ParameterValue<V> {
     private V value;
 
     public ParameterValue(V value) {
+        Preconditions.checkNotNull(value);
         this.value = value;
     }
 
