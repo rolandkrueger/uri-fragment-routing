8aaaf4eefb1078e659a9e64ef2f795baf132a971
Roland Krüger <info@rolandkrueger.info>
2016-01-29 15:04:47 +0100

    Annotations for configuring an action command object

---
+++ src/main/java/org/roklib/webapps/uridispatching/parameter/annotation/CapturedParameter.java
@@ -0,0 +1,13 @@
+package org.roklib.webapps.uridispatching.parameter.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author Roland Krüger
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Documented
+public @interface CapturedParameter {
+    String name();
+}
