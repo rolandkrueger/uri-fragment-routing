7e4f668311e635a669697e105b7dcf2ecdfdcc1e
rolandkrueger <info@rolandkrueger.info>
2016-01-30 20:01:01 +0100

    Add exception classes

---
+++ src/main/java/org/roklib/webapps/uridispatching/exception/InvalidActionCommandClassException.java
@@ -0,0 +1,10 @@
+package org.roklib.webapps.uridispatching.exception;
+
+/**
+ * @author Roland Krüger
+ */
+public class InvalidActionCommandClassException extends RuntimeException {
+    public InvalidActionCommandClassException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/roklib/webapps/uridispatching/exception/InvalidMethodSignatureException.java
@@ -0,0 +1,10 @@
+package org.roklib.webapps.uridispatching.exception;
+
+/**
+ * @author Roland Krüger
+ */
+public class InvalidMethodSignatureException extends RuntimeException {
+    public InvalidMethodSignatureException(String message) {
+        super(message);
+    }
+}
