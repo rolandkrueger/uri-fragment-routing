e0242a43e8f07a36d4f7811556f55bf10a979a2b
Roland Krüger <info@rolandkrueger.info>
2016-02-01 06:32:24 +0100

    Fix test

--- src/test/java/org/roklib/webapps/uridispatching/parameter/AbstractURIParameterTest.java
+++ src/test/java/org/roklib/webapps/uridispatching/parameter/AbstractURIParameterTest.java
@@ -22,57 +22,21 @@ public abstract class AbstractURIParameterTest<V extends Serializable> {
 
     public abstract V getDefaultValue();
 
+    @Before
+    public void setUp() {
+        testObj = getTestURIParameter();
+    }
+
     @Test
     public abstract void testGetSingleValueCount();
 
     @Test
     public abstract void testGetParameterNames();
 
-    @Before
-    public void setUp() {
-        testObj = getTestURIParameter();
-    }
-
-
     @Test
     public void testSetOptional() {
+        assertFalse(testObj.isOptional());
         testObj.setOptional(getDefaultValue());
         assertTrue(testObj.isOptional());
-        testObj.setOptional(getDefaultValue());
-        assertFalse(testObj.isOptional());
-    }
-
-    @SuppressWarnings("serial")
-    private static class TURLParameter extends AbstractURIParameter<String> {
-        boolean parameterized = false;
-
-        public TURLParameter(String id) {
-            super(id);
-        }
-
-        public boolean parameterized() {
-            return parameterized;
-        }
-
-        @Override
-        protected ParameterValue<String> consumeParametersImpl(Map<String, List<String>> parameters) {
-            return null;
-        }
-
-        public URIActionCommand getErrorCommandIfInvalid() {
-            return null;
-        }
-
-        public void parameterizeURIHandler(AbstractURIPathSegmentActionMapper handler) {
-            parameterized = true;
-        }
-
-        public int getSingleValueCount() {
-            return 1;
-        }
-
-        public List<String> getParameterNames() {
-            return null;
-        }
     }
 }
