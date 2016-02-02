3d6e5e4af0e36d0e45e4b3a59610c10e528643e7
Roland Krüger <roland.krueger@oio.de>
2016-02-02 21:16:48 +0100

    Add new parameter type

---
+++ src/main/java/org/roklib/webapps/uridispatching/parameter/StringListUriParameter.java
@@ -0,0 +1,44 @@
+package org.roklib.webapps.uridispatching.parameter;
+
+import org.roklib.webapps.uridispatching.mapper.AbstractURIPathSegmentActionMapper;
+import org.roklib.webapps.uridispatching.parameter.value.ParameterValue;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A URI parameter that takes a list of Strings as its value.
+ *
+ * @author Roland Krüger
+ */
+public class StringListUriParameter extends AbstractURIParameter<List<String>> {
+
+    public StringListUriParameter(String id) {
+        super(id);
+    }
+
+    @Override
+    protected ParameterValue<List<String>> consumeParametersImpl(Map<String, List<String>> parameters) {
+        if (parameters.containsKey(getId())) {
+            return ParameterValue.forValue(new ArrayList<>(parameters.get(getId())));
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void parameterizeURIHandler(AbstractURIPathSegmentActionMapper handler) {
+    }
+
+    @Override
+    public int getSingleValueCount() {
+        return 1;
+    }
+
+    @Override
+    public List<String> getParameterNames() {
+        return Collections.singletonList(getId());
+    }
+}
---
+++ src/test/java/org/roklib/webapps/uridispatching/parameter/StringListUriParameterTest.java
@@ -0,0 +1,41 @@
+package org.roklib.webapps.uridispatching.parameter;
+
+import org.hamcrest.core.IsCollectionContaining;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author rkrueger
+ */
+public class StringListUriParameterTest extends AbstractURIParameterTest<List<String>> {
+
+    @Override
+    public AbstractURIParameter<List<String>> getTestURIParameter() {
+        return new StringListUriParameter("list");
+    }
+
+    @Override
+    public List<String> getTestValue() {
+        return Arrays.asList("a", "b", "c");
+    }
+
+    @Override
+    public List<String> getDefaultValue() {
+        return Arrays.asList("default", "value");
+    }
+
+    @Override
+    public void testGetSingleValueCount() {
+        assertThat(getTestURIParameter().getSingleValueCount(), is(equalTo(1)));
+    }
+
+    @Override
+    public void testGetParameterNames() {
+        assertThat(getTestURIParameter().getParameterNames(), IsCollectionContaining.hasItems("list"));
+    }
+}
